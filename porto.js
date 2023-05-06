8eddea764846d324887c610a494b12409c740d43
GeryYulianto <109521032+GeryYulianto@users.noreply.github.com>
2023-05-06 15:40:39 +0700

    Add form functionality

--- porto.js
+++ porto.js
@@ -15,3 +15,32 @@ tab = (value) => {
         edu.style.display = 'block'
     }
 }
+
+function sendMail() {
+
+    var params = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value, 
+        message: document.getElementById('message').value, 
+    }
+    const serviceID = 'service_5feew3f'
+    const templateID = 'template_l6l9vj7'
+
+    emailjs
+    .send(serviceID, templateID, params)
+    .then(
+        res => {
+            document.getElementById('name').value = "";
+            document.getElementById('email').value = "";
+            document.getElementById('message').value = "";
+            console.log(res)
+            alert('Message sent succesfully')
+        })
+    .catch((err) => console.log(err))
+}
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // prevent default form submission behavior
+
+    sendMail();
+});
